9998c84f1d2804ccb57fda2702ca563b57d43aed
BenioGoshima <rstoyo.benio.goshima@gmail.com>
2023-08-28 12:45:12 +0900

    テストコードの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,22 @@ class User < ApplicationRecord
          has_many :items
          has_many :orders
 
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birth_date, presence: true
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+         KANA_REGEX = /\A[ァ-ヶ一]+\z/.freeze
+
+         validates :nickname, presence: true
+         validates :email, presence: true
+         validates :password, presence: true, format: { with: PASSWORD_REGEX }
+         validates :encrypted_password, presence: true
+
+      with_options presence: true, format: { with: NAME_REGEX } do
+         validates :first_name#, format: { with: NAME_REGEX }
+         validates :last_name#, format: { with: NAME_REGEX }
+      end
+      with_options presence: true, format: { with: KANA_REGEX } do
+        validates :first_name_kana
+        validates :last_name_kana
+      end
+         validates :birth_date, presence: true
 end
