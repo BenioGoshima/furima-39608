7122223bfd942609c4e7a1edafb6df857699c3a7
BenioGoshima <rstoyo.benio.goshima@gmail.com>
2023-08-29 17:24:59 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,21 @@ class ItemsController < ApplicationController
     
   end
 
+  def new
+    @item = Item.new
+  end
+  
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:trade_name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :date_id, :price, :image).merge(user_id: current_user.id)
   end
 end
