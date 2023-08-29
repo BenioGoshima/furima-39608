6914fabfc65ad30856b09820c9a73a0e96f24042
BenioGoshima <rstoyo.benio.goshima@gmail.com>
2023-08-29 11:58:21 +0900

    ActiveHashの導入とアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  validates :trade_name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :postage_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :date_id, numericality: { other_than: 1 }
+  validates :price, presence: true
+
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :condition
+    belongs_to :postage
+    belongs_to :prefecture
+    belongs_to :date
 end
