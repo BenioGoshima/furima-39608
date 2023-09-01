70c6bc31cfcf7b2c5fde2a38b4112b450989d5ec
BenioGoshima <rstoyo.benio.goshima@gmail.com>
2023-09-01 14:31:07 +0900

    単体テストコードの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,20 @@
 class Item < ApplicationRecord
-  validates :trade_name, presence: true
-  validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :postage_id, numericality: { other_than: 1 }
+  validates :trade_name,    presence: true
+  validates :description,   presence: true
+  validates :category_id,   numericality: { other_than: 1 }
+  validates :condition_id,  numericality: { other_than: 1 }
+  validates :postage_id,    numericality: { other_than: 1 }
   validates :prefecture_id, numericality: { other_than: 1 }
-  validates :date_id, numericality: { other_than: 1 }
-  validates :price, presence: true
+  validates :date_id,       numericality: { other_than: 1 }
+  validates :price,         presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :image,         presence: true
 
   belongs_to :user
   has_one_attached :image
+
+  def was_attached?
+    self.image.attached?
+  end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,84 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '新規商品登録' do
+    context '新規商品登録できる場合'do
+      it 'trade_name,description,category_id,condition_id,postage_id,prefecture_id,date_id,price,imageが存在すれば商品登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '新規商品登録できない場合' do
+      it 'trade_nameが空では登録できない。' do
+        @item.trade_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Trade name can't be blank")
+      end
+
+      it 'descriptionが空では登録できない。' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'category_idが選択されていないと登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+
+      it 'condition_idが選択されていないと登録できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
+      end
+
+      it 'postage_idが選択されていないと登録できない' do
+        @item.postage_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage must be other than 1")
+      end
+
+      it 'prefecture_idが選択されていないと登録できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+
+      it 'date_idが選択されていないと登録できない' do
+        @item.date_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Date must be other than 1")
+      end
+
+      it 'priceが空では登録できない。' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'priceが300円以下では登録できない' do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it 'priceが9999999円以上では登録できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
+      it 'image空では登録できない。' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+    end
+  end
+
 end
