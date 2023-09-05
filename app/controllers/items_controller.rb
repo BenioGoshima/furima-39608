f612702996206c001c7f7b54a45e5df92cf26204
BenioGoshima <rstoyo.benio.goshima@gmail.com>
2023-09-05 18:14:13 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,6 +37,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
   private
   def item_params
     params.require(:item).permit(:trade_name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :date_id, :price, :image).merge(user_id: current_user.id)
