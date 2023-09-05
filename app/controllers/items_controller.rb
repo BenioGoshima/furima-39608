01028253b420116a2863a28d9160cffa2fc16007
BenioGoshima <rstoyo.benio.goshima@gmail.com>
2023-09-05 16:52:56 +0900

    ユーザーの遷移先の制御

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:edit, :update, :destroy]
+  before_action :check_owner, only: [:edit, :update, :destroy]
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -37,5 +39,15 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:trade_name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :date_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def check_owner
+    unless user_signed_in? && @item.user == current_user
+      redirect_to root_path
+    end
   end
 end
