716314343c0b381c448685ff164d2fe53b7b3afb
BenioGoshima <rstoyo.benio.goshima@gmail.com>
2023-09-05 17:13:07 +0900

    ログインしていないユーザーの遷移先の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit, :update]
   before_action :set_item, only: [:edit, :update, :destroy]
   before_action :check_owner, only: [:edit, :update, :destroy]
   def index
