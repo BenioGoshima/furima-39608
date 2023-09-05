8968fb80897214e04d667d24b60bb9be95eb3df9
BenioGoshima <rstoyo.benio.goshima@gmail.com>
2023-09-05 18:02:53 +0900

    before_actionの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
-  before_action :set_item, only: [:edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :check_owner, only: [:edit, :update, :destroy]
-  before_action :find_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -44,10 +43,6 @@ class ItemsController < ApplicationController
   end
 
   def set_item
-    @item = Item.find(params[:id])
-  end
-
-  def find_item
     @item = Item.find(params[:id])
   end
 
