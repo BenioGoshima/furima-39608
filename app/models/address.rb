dda876efccb24025ad65437513c8472dbebf76d9
BenioGoshima <rstoyo.benio.goshima@gmail.com>
2023-09-06 15:51:55 +0900

    バリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,13 @@
 class Address < ApplicationRecord
+  varidates :postal_code,    presence: true
+  varidates :prefecture_id,  numericality: { other_than: 1 }
+  varidates :municipalities, presence: true
+  varidates :house_number,   presence: true
+  varidates :building_name,
+  varidates :phone_number,   presence: true
+
+  belongs_to :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :prefecture
 end
