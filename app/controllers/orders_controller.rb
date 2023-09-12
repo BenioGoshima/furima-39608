35ee3751956c2476c8fcd73cdffbad84dc93c266
BenioGoshima <rstoyo.benio.goshima@gmail.com>
2023-09-12 17:19:47 +0900

    フォームの反映

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,22 @@ class OrdersController < ApplicationController
 
 
   def new
-    @item = Item.find(params[:item_id])
+    @address_order = AddressOrder.new
+  end
+
+  def create
+    @address_order =AddressOrder.new(order_params)
+    if @address_order.valid?
+      @address_order.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:address_order).permit(:postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number).merge(user_id: current_user.id)
   end
 end
